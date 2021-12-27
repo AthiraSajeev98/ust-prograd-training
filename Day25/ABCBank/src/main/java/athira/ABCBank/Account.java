5f0faab892792d3c5f11a6d8e73dda4cf977d837
Athira Sajeev <sajeevathira2998@gmail.com>
2021-12-27 23:38:47 +0530

    adding MVC-ABCBank Application with login and account details and other functionalities in progress

---
+++ Day25/ABCBank/src/main/java/athira/ABCBank/Account.java
@@ -0,0 +1,32 @@
+package athira.ABCBank;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class Account {
+
+    ArrayList<String> accountNameList = new ArrayList<String>(List.of("Athira Sajeev", "Adhav Arun"));
+    ArrayList<String> accountNumberList = new ArrayList<String>(List.of("32106753904", "42064918964"));
+
+
+
+    @Autowired
+    public Customer customerAccount;
+
+    public void displayAccountOfCustomer(Customer customer) {
+        customerAccount = customer;
+        if (accountNumberList.contains(customerAccount.accountNumber) && accountNameList.contains(customerAccount.customerName)) {
+            customerAccount.displayCustomerAccount(customerAccount);
+            System.out.println("Account Balance : " + customerAccount.getAccountBalance());
+        }
+
+    }
+
+}
+//Account:To store information about the existing accounts of the bank
